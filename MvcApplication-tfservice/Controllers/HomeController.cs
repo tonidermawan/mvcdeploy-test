74dce159383d07a1da815550b474c2787dcf7358
toni.dermawan <toni_d_y@yahoo.co.id>
2013-02-01 11:29:28 +0700

    initial commit

---
+++ MvcApplication-tfservice/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication_tfservice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!(new feature)";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
